637eee8ea23381689b6d0a163171c40447aee268
Rahmi Pruitt <rjpruitt16@gmail.com>
2016-10-02 17:10:24 -0500

    Created HelloWorld for my starter project
    At this point, the program builds correctly.

---
+++ helloworld/src/main/java/com/example/helloworld/MyBean.java
@@ -0,0 +1,13 @@
+/** The object model for the data we are sending through endpoints */
+public class MyBean {
+
+  private String myData;
+
+  public String getData() {
+    return myData;
+  }
+
+  public void setData(String data) {
+    myData = data;
+  }
+}
